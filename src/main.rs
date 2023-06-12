827d60c48c4a92a85467ed4858b940e91c8d310e
ameyakore <kore.ameya@gmail.com>
2023-06-12 23:38:44 +0530

    chore: changed values for prod

--- src/main.rs
+++ src/main.rs
@@ -134,7 +134,7 @@ async fn main() -> Result<(), NameserverError> {
             .service(register)
             .service(hello)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await?;
     Ok(())
